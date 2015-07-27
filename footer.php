a8e604fa2ee5c45291231d0c26282c249a04d89a
clayton beraldi <c.beraldi@bol.com.br>
2015-07-26 23:26:13 -0300

    centralizando footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer ">
+<footer class="footer">
     <div class="container">
         <p class="text-muted" style="text-align:center;margin-top:50px;">Todos os direitos reservados - 2015</p>
     </div>
