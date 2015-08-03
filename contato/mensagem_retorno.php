91c2c038ba7d84aecb180cad05f21eec5c8c6eff
claytonlima <limaberaldi@gmail.com>
2015-08-02 22:42:59 -0300

    retirando o get do index

--- contato/mensagem_retorno.php
+++ contato/mensagem_retorno.php
@@ -1,17 +1,5 @@
 <meta charset="utf-8">
 <?php
-// Verifica se a variável $_GET['pagina'] existe
-if (isset($_GET['contato'])) {
-    // Pega o valor da variável $_GET['pagina']
-    $arquivo = $_GET['contato'];
-} else {
-    // Se não existir variável, define um valor padrão
-    $arquivo = 'home.php';
-}
-
-?>
-
-<?php
 $email = $_GET['inputEmail'];
 $nome = $_GET['inputNome'];
 $assunto = $_GET['inputAssunto'];
