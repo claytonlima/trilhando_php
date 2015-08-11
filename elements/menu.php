26925383dfed63cd4ee9183c949767514e8a4fbb
clayton <clayton.beraldi@jacotei.com.br>
2015-08-11 18:54:31 -0300

    acerto

--- elements/menu.php
+++ elements/menu.php
@@ -4,6 +4,6 @@
         <li><a href="/empresa.php">Empresa</a></li>
         <li><a href="/produtos.php">Produtos</a></li>
         <li><a href="/servicos.php">Serviços</a></li>
-        <li><a href="/contato.php">Contato</a></li>
+        <li><a href="?pag=contato.php">Contato</a></li>
     </ul>
 </nav>
