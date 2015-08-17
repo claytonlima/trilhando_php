109e4fbb1b358d9c7bfd8996e1e170bca50c5227
clayton <clayton.beraldi@jacotei.com.br>
2015-08-17 16:27:40 -0300

    trazendo conteudo da pagina

--- views/resultado.php
+++ views/resultado.php
@@ -9,13 +9,16 @@ $conn = conexao();
 $pesquisar = $_POST['pesquisar'];
 
 
-$consulta = $conn->prepare("SELECT pagina,link_pagina FROM paginas where conteudo LIKE :pesquisa");
+$consulta = $conn->prepare("SELECT pagina,link_pagina,conteudo FROM paginas where conteudo LIKE :pesquisa");
 $pesquisar_sql = "%".$pesquisar."%";
 $consulta->bindValue(':pesquisa', $pesquisar_sql);
 $consulta->execute();
 
 foreach($consulta as $reg):
-echo '<td><a target="_blank" href="'.$reg['link_pagina'].'">'.$reg['pagina'].'</a></td><br>';
+echo '<td><a href="'.$reg['link_pagina'].'">'.$reg['pagina'].'</a></td><br>';
+echo  '<p>'.$reg['conteudo'].'</p>';
+
+
 endforeach;
 ?>
 
