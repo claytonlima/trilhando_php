11cdcddaeadd1491d507dc7a2c41ed7b4233ad6e
claytonlima <limaberaldi@gmail.com>
2015-08-17 00:21:15 -0300

    acerto pagina retorno

--- views/resultado.php
+++ views/resultado.php
@@ -1,18 +1,25 @@
-<?php
-include "../conexao/conexao.php";
-
-$conn = conexao();
-echo "#### Pesquisa na tabela #### <br><br>";
-$pesquisar = $_POST['pesquisar'];
-
-
-$resultado = $conn->query("SELECT nome FROM usuario where nome LIKE '%".$pesquisar."%'");
-foreach($resultado as $reg):
-    echo "Nome:" .$reg['nome']. "<br><br>";
-endforeach;
-?>
 <div class="jumbotron">
     <h3 align="center">Resultado de pesquisa</h3>
+    <?php
+    include "../conexao/conexao.php";
+
+    $conn = conexao();
+    echo "#### Pesquisa na tabela #### <br><br>";
+    $pesquisar = $_POST['pesquisar'];
+
+
+    $sql = ("SELECT nome FROM usuario where nome LIKE '%".$pesquisar."%'");
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $resultado  = $stm->fetchAll();
+
+    foreach($resultado as $reg):
+        echo "Nome:" .$reg['nome']. "<br><br>";
+    endforeach;
+    ?>
 </div>
+
+
+
 
 
