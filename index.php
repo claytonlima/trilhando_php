eee610cfe00b03a7ce59c3ad9948377c39f1d452
clayton <clayton.beraldi@jacotei.com.br>
2015-10-29 17:32:20 -0200

    acerto utf-8

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include ("rotas.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="iso-8859-1">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Trilhando o PHP</title>
